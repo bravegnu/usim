7ca162186d8cdacde0b3695c247c51cb9617b78a
Vijay Kumar <vijaykumar@bravegnu.org>
2016-11-02 09:30:27 +0530

    Use separate names for each test out file.
    Create files based on the YAML filename, so that the files don't get
    overwritten.

--- tests/run-core-tests.sh
+++ tests/run-core-tests.sh
@@ -9,14 +9,15 @@ read-0() {
 
 for file in *.yml
 do
-    cat ${file} | shyaml get-value when > test.s
-    echo "bkpt" >> test.s
-    arm-none-eabi-gcc -nostdlib -Ttext=0x0 -mcpu=cortex-m0 test.s -o test.elf > test.build 2>&1
-    arm-none-eabi-objcopy test.elf -O binary test.bin >> test.build 2>&1
-    cat ${file} | shyaml key-values given | ../src/core-test > test.output
+    base=$(basename $file .yml)
+    cat ${file} | shyaml get-value when > ${base}.s
+    echo "bkpt" >> ${base}.s
+    arm-none-eabi-gcc -nostdlib -Ttext=0x0 -mcpu=cortex-m0 ${base}.s -o ${base}.elf > ${base}.build 2>&1
+    arm-none-eabi-objcopy ${base}.elf -O binary test.bin >> ${base}.build 2>&1
+    cat ${file} | shyaml key-values given | ../src/core-test > ${base}.output
     cat ${file} | shyaml key-values-0 then |
     	while read-0 key value; do
-	    grep "${key} ${value}" test.output > /dev/null ||
-		echo "FAILED:" $(shyaml get-value name ${file} < ${file})
+	    grep "${key} ${value}" ${base}.output > /dev/null ||
+	        echo "FAILED:" $(shyaml get-value name ${file} < ${file})
 	done
 done
